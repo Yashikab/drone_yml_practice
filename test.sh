48b76b332bdb4fc3b8d54e16e40add4d4d3cde73
Yashikab <yashikaba@gmail.com>
2020-07-05 13:28:54 +0900

    again

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ echo $REVIEWDOG_GITHUB_API_TOKEN
 curl -X POST \
      -H "Authorization: token ${REVIEWDOG_GITHUB_API_TOKEN}" \
      -d "{\"body\": \"hello world\"}" \
-     https://github.com/Yashikab/drone_yml_practice/pull/15
+     https://api.github.com/repos/Yashikab/drone_yml_practice/pulls/comments/15
