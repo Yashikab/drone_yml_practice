6ed5a7058d477b9e0a9d517fcbb339d8023522cb
Yashikab <yashikaba@gmail.com>
2020-07-05 13:47:22 +0900

    one more

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ python /src/get_token.py
 export REVIEWDOG_GITHUB_API_TOKEN=`cat /src/token.conf`
 echo $REVIEWDOG_GITHUB_API_TOKEN
 curl -X POST \
-     -H 'Content-Type:application/json'
+     -H 'Content-Type:application/json' \
      -d "{\"body\": \"hello world\"}" \
      https://api.github.com/repos/Yashikab/drone_yml_practice/issues/15/comments?access_token=${REVIEWDOG_GITHUB_API_TOKEN}
