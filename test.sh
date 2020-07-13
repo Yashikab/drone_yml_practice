f509901583c3e0ad79ae38cbd17f3793166adeea
Yashikab <yashikaba@gmail.com>
2020-07-14 08:23:22 +0900

    fix

--- test.sh
+++ test.sh
@@ -3,9 +3,10 @@
 python /src/get_token.py
 export REVIEWDOG_GITHUB_API_TOKEN=`cat /src/token.conf`
 echo $REVIEWDOG_GITHUB_API_TOKEN
+echo "https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/${DRONE_PULL_REQUEST}/comments"
 curl -X POST \
-     -H "Authorization: token ${REVIEWDOG_GITHUB_API_TOKEN}"
-     -H "Accept: application/json"
+     -H "Authorization: token ${REVIEWDOG_GITHUB_API_TOKEN}" \
+     -H "Accept: application/json" \
      -H "Content-Type:application/json" \
      -d "{\"body\": \"hello world\"}" \
      "https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/${DRONE_PULL_REQUEST}/comments"
