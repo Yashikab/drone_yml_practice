2d1dafeabf20dee7fcc3cdc29f092ee7cd526c39
Yashikab <yashikaba@gmail.com>
2020-08-23 15:03:13 +0900

    test

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ curl -X POST \
      -H "Authorization: token ${REVIEWDOG_GITHUB_API_TOKEN}" \
      -H "Accept: application/json" \
      -H "Content-Type:application/json" \
-     -d "{\"body\": \"hello \\\\n world!\"}" \
+     -d "{\"body\": \"hello world!\"}" \
      "https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/${DRONE_PULL_REQUEST}/comments"
