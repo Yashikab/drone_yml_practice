6be1ed343fc53b005705b06db0ea586f621bb785
Yashikab <yashikaba@gmail.com>
2020-08-25 08:19:37 +0900

    imp pytest with fizzbuzz

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
-# python3.7.5
-print('hello world yashio!')
+# python 3.7.5
+from module.fizzbuzz import fizzbuzz
+
+
+if __name__ == '__main__':
+    print(fizzbuzz(6))
+    print(fizzbuzz(15))
+    print(fizzbuzz(11))
---
+++ src/test/test_main.py
@@ -0,0 +1,13 @@
+# python3.7.5
+import pytest
+from module.fizzbuzz import fizzbuzz
+
+
+@pytest.mark.parametrize("target_num, expected", [
+    (3, 'fizz'),
+    (5, 'buzz'),
+    (7, '7'),
+    (30, 'fizzbuzz')
+])
+def test_fizzbuzz(target_num, expected):
+    assert fizzbuzz(target_num) == expected
