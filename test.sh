a06f932843705e6791ab3e86f49cd18f6c7c23b6
Yashikab <yashikaba@gmail.com>
2020-09-20 13:00:53 +0900

    test

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ echo $REVIEWDOG_GITHUB_API_TOKEN
 #      -d "{\"body\": \"hello world!\"}" \
 #      "https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/${DRONE_PULL_REQUEST}/comments/42"
 curl \
-  -X PATH \
+  -X POST \
   -H "Authorization: token ${REVIEWDOG_GITHUB_API_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/comments/42 \
