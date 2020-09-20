71a08be65feeadb5564157ab9d9005fd10728993
Yashikab <yashikaba@gmail.com>
2020-09-20 13:02:51 +0900

    final test

--- test.sh
+++ test.sh
@@ -13,5 +13,5 @@ curl \
   -X POST \
   -H "Authorization: token ${REVIEWDOG_GITHUB_API_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/comments/42 \
+  https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/comments \
   -d '{"body":"body"}'
