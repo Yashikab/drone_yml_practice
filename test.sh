9172a4e122c38d1f0301eb7256d960b2621ebffd
Yashikab <yashikaba@gmail.com>
2020-09-20 12:33:42 +0900

    how about

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 python /src/get_token.py
 export REVIEWDOG_GITHUB_API_TOKEN=`cat /src/token.conf`
 echo $REVIEWDOG_GITHUB_API_TOKEN
-curl -X POST \
+curl -X PATCH \
      -H "Authorization: token ${REVIEWDOG_GITHUB_API_TOKEN}" \
-     -H "Accept: application/json" \
+     -H "Accept: application/vnd.github.v3+json" \
      -H "Content-Type:application/json" \
      -d "{\"body\": \"hello world!\"}" \
-     "https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/${DRONE_PULL_REQUEST}/comments"
+     "https://api.github.com/repos/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME}/issues/${DRONE_PULL_REQUEST}/comments/42"
