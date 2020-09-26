8e4960fea7e91a76ad7f59fe1309acbc50b50b4d
Yashikab <yashikaba@gmail.com>
2020-09-27 08:39:43 +0900

    how about this

--- src/pygithub_playground/hello.py
+++ src/pygithub_playground/hello.py
@@ -28,25 +28,29 @@ pr = issue.as_pull_request()
 comment_list = list(issue.get_comments())
 comment_list += list(pr.get_review_comments())
 
+dog_marker = '<sub>reported by [reviewdog](https://github.com/reviewdog/reviewdog) :dog:</sub>'
+
 for comment in comment_list:
     print(comment.id, comment.body)
+    if dog_marker in comment.body:
+        comment.delete()
 
-for review in pr.get_reviews():
-    print(review.id, review.body, review.state)
 
+os.environ["REVIEWDOG_GITHUB_API_TOKEN"] = access_token
+cmd1 = ["flake8", "./src/"]
+cmd2 = [
+    "reviewdog",
+    "-reporter=github-pr-review",
+    "-f=pep8",
+    "-diff=\"git diff master\""]
+pipe = subprocess.Popen(
+    cmd1,
+    stdout=subprocess.PIPE
+)
+reviewdog_result = subprocess.run(
+    cmd2,
+    stdin=pipe.stdout,
+    stdout=subprocess.PIPE
+)
 
-# os.environ["REVIEWDOG_GITHUB_API_TOKEN"] = access_token
-# cmd1 = ["flake8", "./src/"]
-# cmd2 = [
-#     "reviewdog",
-#     "-reporter=github-pr-review",
-#     "-f=pep8",
-#     "-diff=\"git diff master\""]
-# pipe = subprocess.Popen(
-#     cmd1,
-#     stdout=subprocess.PIPE
-# )
-# subprocess.run(
-#     cmd2,
-#     stdin=pipe.stdout
-# )
+print(reviewdog_result)
